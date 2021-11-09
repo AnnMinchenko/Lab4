6aeca99edd8c5873ce4d9c615a9c0cbc5d70a8bc
Анна Минченко <grozka00@mail.ru>
2021-11-09 22:00:49 +0800

    Добавьте файлы проекта.

---
+++ Animals.cs
@@ -0,0 +1,19 @@
+﻿/*
+ Для раздачи домашних животных (вес)
+
+Попугаи (цвет, количество выученных слов)
+Собаки (порода, расстояние начиная с которого начинает игнорировать команды хозяина, длина хвоста)
+Кошки (наличие шерсти, улов мышей в день)
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lab4
+{
+
+}
