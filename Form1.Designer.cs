57c529341683be5cd010c67bd79c05a901d6be4f
Анна Минченко <grozka00@mail.ru>
2021-11-10 14:11:40 +0800

    Оформлена главная форма, добавлена часть логики

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,70 @@ namespace Lab4
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnRefill = new System.Windows.Forms.Button();
+            this.txtInfo = new System.Windows.Forms.RichTextBox();
+            this.txtOut = new System.Windows.Forms.RichTextBox();
+            this.btnGet = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRefill
+            // 
+            this.btnRefill.Location = new System.Drawing.Point(12, 12);
+            this.btnRefill.Name = "btnRefill";
+            this.btnRefill.Size = new System.Drawing.Size(309, 29);
+            this.btnRefill.TabIndex = 0;
+            this.btnRefill.Text = "Перезаполнить";
+            this.btnRefill.UseVisualStyleBackColor = true;
+            this.btnRefill.Click += new System.EventHandler(this.btnRefill_Click);
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtInfo.Location = new System.Drawing.Point(12, 62);
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.ReadOnly = true;
+            this.txtInfo.Size = new System.Drawing.Size(309, 69);
+            this.txtInfo.TabIndex = 1;
+            this.txtInfo.Text = "";
+            // 
+            // txtOut
+            // 
+            this.txtOut.Location = new System.Drawing.Point(12, 148);
+            this.txtOut.Name = "txtOut";
+            this.txtOut.Size = new System.Drawing.Size(195, 120);
+            this.txtOut.TabIndex = 2;
+            this.txtOut.Text = "";
+            // 
+            // btnGet
+            // 
+            this.btnGet.Location = new System.Drawing.Point(224, 148);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(94, 120);
+            this.btnGet.TabIndex = 3;
+            this.btnGet.Text = "Взять";
+            this.btnGet.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(333, 289);
+            this.Controls.Add(this.btnGet);
+            this.Controls.Add(this.txtOut);
+            this.Controls.Add(this.txtInfo);
+            this.Controls.Add(this.btnRefill);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnRefill;
+        private System.Windows.Forms.RichTextBox txtInfo;
+        private System.Windows.Forms.RichTextBox txtOut;
+        private System.Windows.Forms.Button btnGet;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,10 +12,64 @@ namespace Lab4
 {
     public partial class Form1 : Form
     {
+        List<Animal> animalsList = new List<Animal>();
+
         public Form1()
         {
             InitializeComponent();
+            ShowInfo();
         }
 
+        private void btnRefill_Click(object sender, EventArgs e)
+        {
+            this.animalsList.Clear();
+            var rnd = new Random();
+            for (var i = 0; i < 10; ++i)
+            {
+                switch (rnd.Next() % 3) 
+                {
+                    case 0: 
+                        this.animalsList.Add(new Parrot());
+                        break;
+                    case 1: 
+                        this.animalsList.Add(new Dog());
+                        break;
+                    case 2: 
+                        this.animalsList.Add(new Cat());
+                        break;
+                }
+            }
+            ShowInfo();
+        }
+
+        private void ShowInfo()
+        {
+            int parrotCount = 0;
+            int dogCount = 0;
+            int catCount = 0;
+
+            foreach (var fruit in this.animalsList)
+            {
+                if (fruit is Parrot) 
+                {
+                    parrotCount += 1;
+                }
+                else if (fruit is Dog)
+                {
+                    dogCount += 1;
+                }
+                else if (fruit is Cat)
+                {
+                    catCount += 1;
+                }
+            }
+
+            // а ну и вывести все это надо на форму
+            txtInfo.Text = "Попуги\tПесы\tКиты"; // буквы экнмлю, чтобы влезло на форму
+            txtInfo.Text += "\n";
+            txtInfo.Text += String.Format("{0}\t{1}\t{2}", parrotCount, dogCount, catCount);
+        }
     }
+
+    
 }
